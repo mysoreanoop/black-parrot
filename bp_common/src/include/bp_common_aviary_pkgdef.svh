0fcbc20a13ff804e5ec6f42c150b3758b419a09d
sripathi-muralitharan <sripathimuralitharan@gmail.com>
2021-03-26 17:27:57 -0700

    Minor changes to config; Refactoring fill index in the data mem packet

--- bp_common/src/include/bp_common_aviary_pkgdef.svh
+++ bp_common/src/include/bp_common_aviary_pkgdef.svh
@@ -389,6 +389,22 @@
                         ,bp_unicore_cfg_p
                         );
 
+  localparam bp_proc_param_s bp_unicore_l1_sub_bank_mask_override_p =
+    '{dcache_sets: 64
+      ,dcache_assoc: 4
+      ,dcache_block_width: 512
+      ,dcache_fill_width: 64
+      ,icache_sets: 64
+      ,icache_assoc: 8
+      ,icache_block_width: 512
+      ,icache_fill_width: 64
+      ,default       : "inv"
+      };
+  `bp_aviary_derive_cfg(bp_unicore_l1_sub_bank_mask_cfg_p
+                        ,bp_unicore_l1_sub_bank_mask_override_p
+                        ,bp_unicore_cfg_p
+                        );
+  
   localparam bp_proc_param_s bp_unicore_l1_atomic_override_p =
     '{lr_sc                 : e_l1
       ,amo_swap             : e_l1
@@ -419,22 +435,6 @@
       };
   `bp_aviary_derive_cfg(bp_unicore_writethrough_cfg_p
                         ,bp_unicore_writethrough_override_p
-                        ,bp_unicore_cfg_p
-                        );
-
-  localparam bp_proc_param_s bp_unicore_l1_sub_bank_mask_override_p =
-    '{dcache_sets: 64
-      ,dcache_assoc: 4
-      ,dcache_block_width: 512
-      ,dcache_fill_width: 64
-      ,icache_sets: 64
-      ,icache_assoc: 8
-      ,icache_block_width: 512
-      ,icache_fill_width: 64
-      ,default       : "inv"
-      };
-  `bp_aviary_derive_cfg(bp_unicore_l1_sub_bank_mask_cfg_p
-                        ,bp_unicore_l1_sub_bank_mask_override_p
                         ,bp_unicore_cfg_p
                         );
 
@@ -903,8 +903,8 @@
     ,bp_unicore_paddr_large_cfg_p
     ,bp_unicore_writethrough_cfg_p
     ,bp_unicore_l2_atomic_cfg_p
-    ,bp_unicore_l1_sub_bank_mask_cfg_p
     ,bp_unicore_l1_atomic_cfg_p
+    ,bp_unicore_l1_sub_bank_mask_cfg_p
     ,bp_unicore_l1_wide_cfg_p
     ,bp_unicore_l1_hetero_cfg_p
     ,bp_unicore_l1_tiny_cfg_p
@@ -968,8 +968,8 @@
     ,e_bp_unicore_paddr_large_cfg                   = 14
     ,e_bp_unicore_writethrough_cfg                  = 13
     ,e_bp_unicore_l2_atomic_cfg                     = 12
-    ,e_bp_unicore_l1_sub_bank_mask_cfg              = 11
-    ,e_bp_unicore_l1_atomic_cfg                     = 10
+    ,e_bp_unicore_l1_atomic_cfg                     = 11
+    ,e_bp_unicore_l1_sub_bank_mask_cfg              = 10
     ,e_bp_unicore_l1_wide_cfg                       = 9
     ,e_bp_unicore_l1_hetero_cfg                     = 8
     ,e_bp_unicore_l1_tiny_cfg                       = 7
