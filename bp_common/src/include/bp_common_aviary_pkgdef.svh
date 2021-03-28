13ee1070825069081453c2d238bba2e3c835536b
sripathi-muralitharan <sripathimuralitharan@gmail.com>
2021-03-28 10:20:30 -0700

    Fix review comments

--- bp_common/src/include/bp_common_aviary_pkgdef.svh
+++ bp_common/src/include/bp_common_aviary_pkgdef.svh
@@ -389,7 +389,7 @@
                         ,bp_unicore_cfg_p
                         );
 
-  localparam bp_proc_param_s bp_unicore_l1_sub_bank_mask_override_p =
+  localparam bp_proc_param_s bp_unicore_l1_subbank_override_p =
     '{dcache_sets: 64
       ,dcache_assoc: 4
       ,dcache_block_width: 512
@@ -400,8 +400,8 @@
       ,icache_fill_width: 64
       ,default       : "inv"
       };
-  `bp_aviary_derive_cfg(bp_unicore_l1_sub_bank_mask_cfg_p
-                        ,bp_unicore_l1_sub_bank_mask_override_p
+  `bp_aviary_derive_cfg(bp_unicore_l1_subbank_cfg_p
+                        ,bp_unicore_l1_subbank_override_p
                         ,bp_unicore_cfg_p
                         );
   
@@ -904,7 +904,7 @@
     ,bp_unicore_writethrough_cfg_p
     ,bp_unicore_l2_atomic_cfg_p
     ,bp_unicore_l1_atomic_cfg_p
-    ,bp_unicore_l1_sub_bank_mask_cfg_p
+    ,bp_unicore_l1_subbank_cfg_p
     ,bp_unicore_l1_wide_cfg_p
     ,bp_unicore_l1_hetero_cfg_p
     ,bp_unicore_l1_tiny_cfg_p
@@ -969,7 +969,7 @@
     ,e_bp_unicore_writethrough_cfg                  = 13
     ,e_bp_unicore_l2_atomic_cfg                     = 12
     ,e_bp_unicore_l1_atomic_cfg                     = 11
-    ,e_bp_unicore_l1_sub_bank_mask_cfg              = 10
+    ,e_bp_unicore_l1_subbank_cfg                    = 10
     ,e_bp_unicore_l1_wide_cfg                       = 9
     ,e_bp_unicore_l1_hetero_cfg                     = 8
     ,e_bp_unicore_l1_tiny_cfg                       = 7
